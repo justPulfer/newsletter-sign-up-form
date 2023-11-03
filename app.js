1f5804b1e0c32c6b8dd0064728bb09c9f1e4b37d
just_pulfer <djlydia.pd@gmail.com>
2023-11-03 00:02:35 -0600

    completed basic HTML and CSS...next is to implement form validation

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const illustration = document.getElementById("illustration");
+const newsletterCard = document.getElementById("newsletterCard");
+const thankYouCard = document.getElementById("thankYouCard");
+const subscribeBtn = document.getElementById("subscribeBtn");
+const dismissBtn = document.getElementById("dismissBtn");
 
-if (screen.width <= 375) {
+if (screen.width <= 1024) {
 	illustration.src = "/assets/images/illustration-sign-up-mobile.svg";
 }
+
+subscribeBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	newsletterCard.style.display = "none";
+	thankYouCard.style.display = "flex";
+});
+
+dismissBtn.addEventListener("click", (e) => {
+	newsletterCard.style.display = "flex";
+	thankYouCard.style.display = "none";
+});
